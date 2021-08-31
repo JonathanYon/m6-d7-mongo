57838cd1fc2ca9d7b27ae957deac82ab3ec1ad46
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-31 14:54:07 +0200

    all routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
+import blogsRouter from "./service/blog/index.js";
 
 const port = process.env.PORT || 5000;
 const server = express();
 
 server.use(express.json());
+
+server.use("blogs", blogsRouter);
 
 mongoose.connect(process.env.MONGOS_CONNECTION);
 mongoose.connection.on(`Connected!`, () => {
--- src/service/blog/index.js
+++ src/service/blog/index.js
@@ -1,33 +1,59 @@
 import { Router } from "express";
+import blogModel from "./schema.js";
 
 const blogsRouter = Router();
 
 blogsRouter.post("/", async (req, res, next) => {
   try {
+    const post = await blogModel(req.body);
+    const { _id } = await post.save();
+    res.status(201).send({ _id });
   } catch (error) {
     next(error);
   }
 });
 blogsRouter.get("/", async (req, res, next) => {
   try {
+    const posts = await blogModel.find({});
+    res.send(posts);
   } catch (error) {
     next(error);
   }
 });
 blogsRouter.get("/:Id", async (req, res, next) => {
   try {
+    const post = await blogModel.findById(req.params.Id);
+    if (post) {
+      res.send(post);
+    } else {
+      res.send(`blog ${req.params.Id} NOT found!!`);
+    }
   } catch (error) {
     next(error);
   }
 });
 blogsRouter.put("/:Id", async (req, res, next) => {
   try {
+    const post = await blogModel.findByIdAndUpdate(req.params.Id, req.body, {
+      new: true,
+    });
+    if (post) {
+      res.send(post);
+    } else {
+      res.send(`blog ${req.params.Id} NOT found!!`);
+    }
   } catch (error) {
     next(error);
   }
 });
 blogsRouter.delete("/:Id", async (req, res, next) => {
   try {
+    const post = await blogModel.findByIdAndDelete(req.params.Id);
+    if (post) {
+      res.status(204).send(`Deleted!!`);
+    } else {
+      res.send(`${req.params.Id} NOT found!`);
+    }
   } catch (error) {
     next(error);
   }
