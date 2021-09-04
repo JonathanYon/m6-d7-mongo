8ec7d71a05f1dc4122fa53d11d968c4ef511ddae
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-04 21:05:23 +0200

    adding

--- src/server.js
+++ src/server.js
@@ -9,10 +9,12 @@ import {
   notFoundHandler,
 } from "./errorHandlers.js";
 import blogsRouter from "./service/blog/index.js";
-import commentRouter from "./service/comments/index.js";
+// import commentRouter from "./service/comments/index.js";
 
 const port = process.env.PORT || 5000;
 const server = express();
+
+//later add the origins here brp
 
 server.use(express.json());
 server.use(cors());
@@ -24,6 +26,12 @@ server.use(notFoundHandler);
 server.use(badRequestHandler);
 server.use(forbidenHandler);
 server.use(genericErrorHandler);
+
+server.use((req, res) => {
+  if (!req.route) {
+    res.status(404).send("Buddy! check your route 👁👀👀");
+  }
+});
 
 mongoose.connect(process.env.MONGOS_CON_LOCAL);
 mongoose.connection.on(`connected`, () => {
